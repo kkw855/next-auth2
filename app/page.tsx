fcfe207cbbeb22e3258898116ccbcc2e655a389f
kkw855 <kkw855@gmail.com>
2024-05-15 21:34:30 +0900

    ✨ feature: Home 페이지 구현
    shadcn/ui button 설치
    LoginButton 컴포넌트 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { LoginButton } from '@/components/auth/login-button'
+
 export default function Home() {
-  return <main>Hone Page</main>
+  return (
+    <main className='
+    h-full
+    flex flex-col items-center justify-center
+    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+      <div className='text-center text-white space-y-4'>
+        <h1 className='text-6xl font-semibold drop-shadow-md font-poppins'>
+          🔐 Auth
+        </h1>
+        <p className='text-lg'>
+          A simple authentication service
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant='secondary' size='lg'>
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  )
 }
---
+++ components/auth/login-button.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+import { useRouter } from 'next/navigation'
+
+type LoginButton = {
+  children: ReactNode
+  mode?: 'modal' | 'redirect'
+  asChild?: boolean
+}
+
+export const LoginButton = ({ children, mode }: LoginButton) => {
+  const router = useRouter()
+
+  const onClick = () => {
+    router.push('/auth/login')
+  }
+
+  if (mode === 'modal') {
+    return <span>TODO: Implement modal</span>
+  }
+
+  return (
+    <span className='inline-block' onClick={onClick}>
+      {children}
+    </span>
+  )
+}
