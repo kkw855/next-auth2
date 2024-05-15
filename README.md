dee1b7ee2b3f685b47d63ee3790e2775f7e06187
kkw855 <kkw855@gmail.com>
2024-05-15 20:22:05 +0900

    ➕ devDependencies: ESLint 설정
    - ESLint 새로운 flat 설정 파일 적용
    - 여러가지 ESLint Rule 추가 (eslint.config.mjs 파일 참조)

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Next Auth V5
 
+## TODO
+- tsconfig.json 설명 주석 추가하기
+- eslint.config.mjs 설명 주석 추가하기
+
 ## Getting Started
 
 - Node 20.12.2  
@@ -27,4 +31,14 @@ ncu
 # 모든 패키지를 latest 버전으로 업그레이드. package.json 파일만 수정하므로 이후에 
 # 반드시 npm install 명령으로 실제 패키지를 설치해야 함
 ncu --upgrade 
+```
+
+## Migration from ESLint legacy to ESLint flat
+아직 Next.js 에서 새로운 ESLint flat config format 을 지원하지 않아서 수동으로 직접 설정해야 한다.
+```bash
+npm uninstall eslint typescript eslint-config-next
+
+npm install --save-dev eslint @eslint/js typescript typescript-eslint
+
+npm install --save-dev eslint-plugin-react eslint-plugin-react-hooks @next/eslint-plugin-next
 ```
---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+/* eslint-disable */
+// noinspection JSCheckFunctionSignatures
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js'
+import jsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import next from '@next/eslint-plugin-next'
+import globals from 'globals'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  reactRecommended,
+  jsxRuntime,
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: reactHooks.configs.recommended.rules,
+  },
+  {
+    plugins: {
+      '@next/next': next,
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+      ...next.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        plugins: {
+          '@next/next': {
+            rules: next.configs["core-web-vitals"].rules,
+            configs: next.configs,
+          }
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      }
+    },
+    rules: {
+      'react/jsx-sort-props': 'error'
+    }
+  },
+)
