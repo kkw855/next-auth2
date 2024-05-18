9c5dae023f1119a85d8accf93c931f4d64d24023
kkw855 <kkw855@gmail.com>
2024-05-18 12:17:34 +0900

    ✨ feature: SignIn 페이지 이메일 중복 체크, 패스워드 암호화

--- actions/register.ts
+++ actions/register.ts
@@ -1,16 +1,39 @@
 'use server'
 
 import { safeParseAsync } from 'valibot'
+import bcrypt from 'bcrypt'
 
 import { Register } from '@/schemas'
 import type { ServerResponse } from '@/types/response'
+import { users } from '@/db/schema'
+import db from '@/db/db'
+import { getUserByEmail } from '@/db/user'
 
 // Server Action
 // 비동기 함수로 만들어야 함, 오직 HTTP POST method 만 사용
 export const register = async (values: Register): Promise<ServerResponse> => {
   const validateFields = await safeParseAsync(Register, values)
 
-  return validateFields.success
-    ? { _tag: 'success', message: 'Email sent!' }
-    : { _tag: 'error', message: 'Invalid register fields' }
+  // 유효성 검사
+  if (!validateFields.success) return { _tag: 'error', message: 'Invalid fields!' }
+
+  const { name, email, password } = validateFields.output
+
+  // Email 중복 체크
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) return { _tag: 'error', message: 'Email already in use!' }
+
+  // 비밀번호 암호화
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  await db.insert(users).values({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  // TODO: 인증 토큰 이메일 보내기
+
+  return { _tag: 'success', message: 'User created!' }
 }
--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,41 @@
-import { pgTable, text } from 'drizzle-orm/pg-core'
+import { pgTable, primaryKey, text, timestamp, integer } from 'drizzle-orm/pg-core'
+import type { AdapterAccountType } from '@auth/core/adapters'
 import { createId } from '@paralleldrive/cuid2'
 
 export const users = pgTable('user', {
-  id: text('id').$defaultFn(() => createId()),
-  name: text('name')
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  name: text('name').notNull(),
+  email: text('email')
+    .unique()
+    .notNull(),
+  emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  image: text('image'),
+  password: text('password').notNull() // Auth.js 에 없는 커스텀 필드 추가
 })
+export type User = typeof users.$inferSelect
+
+export const accounts = pgTable(
+  'account',
+  {
+    userId: text('userId')
+      .notNull()
+      .references(() => users.id, { onDelete: 'cascade' }),
+    type: text('type').$type<AdapterAccountType>().notNull(),
+    provider: text('provider').notNull(),
+    providerAccountId: text('providerAccountId').notNull(),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: integer('expires_at'),
+    token_type: text('token_type'),
+    scope: text('scope'),
+    id_token: text('id_token'),
+    session_state: text('session_state'),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
+)
